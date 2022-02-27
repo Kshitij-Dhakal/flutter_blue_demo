4c423b6d390634de64a3e91c5a07fa2126b086da
Kshitij-Dhakal <TW*PHwDat2nvM*c5Y^pQ#ZhrurcUXjr3GzPQ#3ezfFwXE3J5ANwyK&SbaYMX>
2022-02-27 23:28:43 +0545

    flutter blue demo

---
+++ lib/blue_page.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+class BluePage extends StatefulWidget {
+  const BluePage({Key? key}) : super(key: key);
+
+  @override
+  _BluePageState createState() => _BluePageState();
+}
+
+class _BluePageState extends State<BluePage> {
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  StreamSubscription<List<ScanResult>>? scanSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    scan();
+  }
+
+  @override
+  void dispose() {
+    scanSubscription?.cancel();
+    super.dispose();
+  }
+
+  scan() {
+    // Start scanning
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
+
+// Listen to scan results
+    scanSubscription = flutterBlue.scanResults.listen((results) {
+      // do something with scan results
+      for (ScanResult r in results) {
+        print('${r.device.name} found! rssi: ${r.rssi}');
+      }
+    });
+
+// Stop scanning
+    flutterBlue.stopScan();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scanning Devices'),
+      ),
+    );
+  }
+}
